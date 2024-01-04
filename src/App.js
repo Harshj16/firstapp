5576b25c815e39ab2a04dacba1147fd2cd769aa0
Harsh Joshi <joshiharshjayesh@gmail.com>
2024-01-04 20:35:51 +0530

    Learning React Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 // import logo from './logo.svg'
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 // import Button from './Button'
 // import { Heading2 } from './Heading'
 // import Counter from './State'
-import Login from './Login'
+// import Login from './Login'
+import Home from './Home'
+import About from './About'
+import { BrowserRouter as Routes, Route, Link } from 'react-router-dom'
 
 function App () {
-  const [info] = useState()
+  // const [info] = useState()
 
   return (
     <div>
-      <Login props={info} />
+      <nav>
+        <Link to='/' className='nav-item'>
+          Homepage
+        </Link>
+        <Link to='/about' className='nav-item'>
+          About Little Lemon
+        </Link>
+        <Link to='/contact' className='nav-item'>
+          Contact
+        </Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+      </Routes>
     </div>
   )
 }
